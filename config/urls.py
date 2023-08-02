2cc1b9ce2c3a4b5815e6121a7f7f2ebb40d2601b
nomura3656 <138486255+nomura3656@users.noreply.github.com>
2023-08-02 10:25:18 +0000

    nomura 1

---
+++ config/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+        path('', TemplateView.as_view(template_name='index.html'), name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
